81b7b79483bdd5e396ac07f4b3d73201493ff591
Владислав Бобошко <Boboshko Vladyslav@rivne.itstep.org>
2019-07-18 12:13:08 +0300

    Add project files.

---
+++ UserApp/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserApp.Models;
+
+namespace UserApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Register");
+        }
+
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (EFContext context = new EFContext())
+                {
+                    Address address = new Address()
+                    {
+                        Country = model.Country,
+                        Apartement = model.Apartement,
+                        City = model.City,
+                        Street = model.Street,
+                    };
+                    User user = new User()
+                    {
+                        Email = model.Email,
+                        Name = model.Name,
+                        Password = model.Password,
+                        Phone = model.Phone,
+                        DateOfBirth = model.DateOfBirth,
+                        AddressOf = address
+                    };
+                    Product product = new Product()
+                    {
+                        Name="Вермешель",
+                        Price=13.99
+                    };
+                    context.Products.Add(product);
+                    context.Users.Add(user);
+                    context.SaveChanges();
+                }
+                ViewBag.IsSuccessed = true;
+                return View(new RegisterViewModel());
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
---
+++ UserApp/Models/RegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UserApp.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [DataType(DataType.Date), Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required,Compare("Password")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string Apartement { get; set; }
+
+    }
+}
